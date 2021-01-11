afa4e9430c9bee019157721f6d29033b19ada645
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-01-11 22:00:14 +0200

    complete challenge #06: Add a maximum limit of £90 to my card

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
   attr_reader :balance
-  
+
+  MAXIMUM_BALANCE = 90
   def initialize
     @balance = 0
   end
 
   def top_up(amount_of_money)
+    raise 'Cannot hold more than 90' if @balance + amount_of_money > MAXIMUM_BALANCE
+
     @balance += amount_of_money
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,8 +7,16 @@ describe Oystercard do
   it 'has a balance of zero' do
     expect(subject.balance).to eq 0
   end
- 
   it 'tops up the balance' do
     expect { subject.top_up(5) }.to change(subject, :balance).by(5)
+  end
+  context 'when maximum limit is exceeded' do
+    it 'raises error' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+
+      subject.top_up(90)
+
+      expect { subject.top_up(1) }.to raise_error "Cannot hold more than #{maximum_balance}"
+    end
   end
 end
